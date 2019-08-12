131d27e7d2052e76a754737336b59e9623bc31cc
from1998 <1967236098@qq.com>
2019-08-12 09:46:43 +0800

    退出登录完成

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,16 +1,15 @@
-$('#logout').on('click', function () {
-    var isConfirm = confirm('您真的要退出吗?');
+$('#logout').on('click', function() {
+    var isConfirm = confirm('确定要退出?')
     if (isConfirm) {
-      // alert('用户点击了确认按钮')
-      $.ajax({
-        type: 'post',
-        url: '/logout',
-        success: function () {
-          location.href = 'login.html';
-        },
-        error: function () {
-          alert('退出失败')
-        }
-      })
+        $.ajax({
+            type: 'post',
+            url: '/logout',
+            success: function() {
+                location.href = 'login.html'
+            },
+            error: function() {
+                alert('退出失败')
+            }
+        })
     }
-  });
+})
